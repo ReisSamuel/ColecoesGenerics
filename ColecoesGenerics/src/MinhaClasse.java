d64151a69a3772e98807c0018769dbb10fcf70a1
ice <ice@lab06m09.nrc.ice.ufjf.br>
2023-02-09 20:38:38 -0300

    add exemplo de generics

--- ColecoesGenerics/src/MinhaClasse.java
+++ ColecoesGenerics/src/MinhaClasse.java
@@ -1,13 +1,21 @@
 
 public class MinhaClasse {
 	public int num;
+	public double num1;
 	
 	public MinhaClasse(int num) {
 		this.num = num;
 	}
 	
+	public MinhaClasse(double num1) {
+		this.num1 = num1;
+	}
+	
 	public int aoQuadrado() {
-		return num*num;
+		return num * num;
 	}
 
+	public double aoQuadradoDouble() {
+		return num1 * num1;
+	}
 }
